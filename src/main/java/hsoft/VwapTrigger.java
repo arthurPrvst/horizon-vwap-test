18567c34f45f2c52ce1bdaeaa3005dbfd2503072
ArthurProvost <hatezgroove@gmail.com>
2021-09-16 12:31:28 +0200

    format

--- src/main/java/hsoft/VwapTrigger.java
+++ src/main/java/hsoft/VwapTrigger.java
@@ -6,7 +6,6 @@ import org.apache.log4j.LogManager;
 public class VwapTrigger {
 
   public static void main(String[] args) {
-
     DataListenerService handler = new DataListenerService();
     handler.listen();
     // When this method returns, the test is finished and you can check your results in the console

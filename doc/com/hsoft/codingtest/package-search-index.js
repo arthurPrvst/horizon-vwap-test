b0bd5efe30bfd927f737eaa10753a4741f875517
ArthurProvost <hatezgroove@gmail.com>
2021-09-16 20:30:19 +0200

    generate jdoc

---
+++ doc/com/hsoft/codingtest/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"hsoft"},{"l":"hsoft.models"},{"l":"hsoft.services"}]
---
+++ doc/com/hsoft/codingtest/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"hsoft.services","l":"DataListenerService"},{"p":"hsoft.models","l":"Execution"},{"p":"hsoft.models","l":"ExecutionRecords"},{"p":"hsoft","l":"ExecutionsTest"},{"p":"hsoft","l":"FairValueTest"},{"p":"hsoft","l":"VwapTest"},{"p":"hsoft","l":"VwapTrigger"},{"p":"hsoft","l":"WrongDataTest"}]
